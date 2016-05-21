89256c6c7e13f73752f09552a265882d9f863e53
Alex <zhukovich.a@gmail.com>
2016-05-21 18:31:10 +0200

    Updated CoffeeOrder

--- app/src/main/java/com/alexzh/simplecoffeeorder/CoffeeOrder.java
+++ app/src/main/java/com/alexzh/simplecoffeeorder/CoffeeOrder.java
@@ -1,5 +1,9 @@
 package com.alexzh.simplecoffeeorder;
 
+import com.alexzh.simplecoffeeorder.model.Coffee;
+
+import java.util.HashMap;
+
 public class CoffeeOrder {
     private float mCoffeePrice;
     private int mCoffeeCount;
@@ -42,4 +46,33 @@ public class CoffeeOrder {
         mTotalPrice = mCoffeePrice * mCoffeeCount;
     }
 
+    public static float calculateTotalPrice(HashMap<Coffee, Integer> orderHashMap) {
+        float total = 0.0f;
+        if (orderHashMap != null) {
+            for (Coffee key : orderHashMap.keySet()) {
+                total += key.getPrice() * orderHashMap.get(key);
+            }
+        }
+        return total;
+    }
+
+    public static Coffee getCoffeeByPosition(HashMap<Coffee, Integer> orderedHashMap, int position) {
+        int interactionPosition = 0;
+        for (Coffee key : orderedHashMap.keySet()) {
+            if (interactionPosition == position) {
+                return  key;
+            }
+            interactionPosition++;
+        }
+        return null;
+    }
+
+    public static int getCountByCoffee(HashMap<Coffee, Integer> orderedHashMap, Coffee coffee) {
+        for (Coffee key : orderedHashMap.keySet()) {
+            if (key.equals(coffee)) {
+                return  orderedHashMap.get(key);
+            }
+        }
+        return 0;
+    }
 }
