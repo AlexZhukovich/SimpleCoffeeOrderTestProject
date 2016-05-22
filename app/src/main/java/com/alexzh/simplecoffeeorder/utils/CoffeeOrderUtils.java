da7f25f8bba8f2ad472a6734a2d123b27e21bda0
Alex <zhukovich.a@gmail.com>
2016-05-22 12:44:39 +0200

    Created CoffeeOrderUtils class

--- app/src/main/java/com/alexzh/simplecoffeeorder/utils/CoffeeOrderUtils.java
+++ app/src/main/java/com/alexzh/simplecoffeeorder/utils/CoffeeOrderUtils.java
@@ -1,7 +1,28 @@
 package com.alexzh.simplecoffeeorder.utils;
 
-/**
- * Created by alex on 5/22/16.
- */
+import com.alexzh.simplecoffeeorder.model.Coffee;
+
+import java.util.HashMap;
+
 public class CoffeeOrderUtils {
+
+    public static Coffee getCoffeeByPosition(HashMap<Coffee, Integer> orderedHashMap, int position) {
+        int interactionPosition = 0;
+        for (Coffee key : orderedHashMap.keySet()) {
+            if (interactionPosition == position) {
+                return  key;
+            }
+            interactionPosition++;
+        }
+        return null;
+    }
+
+    public static int getCountByCoffee(HashMap<Coffee, Integer> orderedHashMap, Coffee coffee) {
+        for (Coffee key : orderedHashMap.keySet()) {
+            if (key.equals(coffee)) {
+                return  orderedHashMap.get(key);
+            }
+        }
+        return 0;
+    }
 }
