32a96d55a0f689971d0727b8efe2a18f44e64919
Alex <zhukovich.a@gmail.com>
2016-05-23 13:57:06 +0200

    Added function and improved test code

--- app/src/androidTest/java/com/alexzh/simplecoffeeorder/ui/MainActivityTest.java
+++ app/src/androidTest/java/com/alexzh/simplecoffeeorder/ui/MainActivityTest.java
@@ -62,44 +62,38 @@ public class MainActivityTest {
 
     @Test
     public void shouldOrderEspressoAndCafeZorro() {
-        int espressoCount = 0;
-        int coffeeZoroCount = 0;
+        int coffeeCount = 0;
         float totalCoffeePrice = 0.0f;
-        final List<Coffee> coffeeList = CoffeeService.getCoffeeData();
-        final String espresso = coffeeList.get(0).getName();
-        final float espressoPrice = coffeeList.get(0).getPrice();
+        final String espresso = mCoffeeList.get(0).getName();
+        final float espressoPrice = mCoffeeList.get(0).getPrice();
 
-        final String coffeeZoro = coffeeList.get(7).getName();
-        final float coffeeZoroPrice = coffeeList.get(7).getPrice();
+        final String coffeeZoro = mCoffeeList.get(7).getName();
+        final float coffeeZoroPrice = mCoffeeList.get(7).getPrice();
 
-        clickToViewChildItem(R.id.recyclerView, espresso, R.id.coffee_increment);
-        checkTextViewCountForCoffee(R.id.recyclerView, R.id.coffee_count, espresso, String.valueOf(++espressoCount));
         totalCoffeePrice += espressoPrice;
-        onView(withId(R.id.total_price_toolbar)).check(matches(withText(getString(mActivityRule, R.string.price, totalCoffeePrice))));
+        changeToCoffeeOrderCountAndVerify(espresso, ++coffeeCount, totalCoffeePrice, R.id.coffee_increment);
 
-        clickToViewChildItem(R.id.recyclerView, espresso, R.id.coffee_increment);
-        checkTextViewCountForCoffee(R.id.recyclerView, R.id.coffee_count, espresso, String.valueOf(++espressoCount));
         totalCoffeePrice += espressoPrice;
-        onView(withId(R.id.total_price_toolbar)).check(matches(withText(getString(mActivityRule, R.string.price, totalCoffeePrice))));
+        changeToCoffeeOrderCountAndVerify(espresso, ++coffeeCount, totalCoffeePrice, R.id.coffee_increment);
 
-        clickToViewChildItem(R.id.recyclerView, espresso, R.id.coffee_decrement);
-        checkTextViewCountForCoffee(R.id.recyclerView, R.id.coffee_count, espresso, String.valueOf(--espressoCount));
         totalCoffeePrice -= espressoPrice;
-        onView(withId(R.id.total_price_toolbar)).check(matches(withText(getString(mActivityRule, R.string.price, totalCoffeePrice))));
+        changeToCoffeeOrderCountAndVerify(espresso, --coffeeCount, totalCoffeePrice, R.id.coffee_decrement);
 
-        clickToViewChildItem(R.id.recyclerView, espresso, R.id.coffee_decrement);
-        checkTextViewCountForCoffee(R.id.recyclerView, R.id.coffee_count, espresso, String.valueOf(--espressoCount));
         totalCoffeePrice -= espressoPrice;
-        onView(withId(R.id.total_price_toolbar)).check(matches(withText(getString(mActivityRule, R.string.price, totalCoffeePrice))));
+        changeToCoffeeOrderCountAndVerify(espresso, --coffeeCount, totalCoffeePrice, R.id.coffee_decrement);
 
-        clickToViewChildItem(R.id.recyclerView, coffeeZoro, R.id.coffee_increment);
-        checkTextViewCountForCoffee(R.id.recyclerView, R.id.coffee_count, coffeeZoro, String.valueOf(++coffeeZoroCount));
         totalCoffeePrice += coffeeZoroPrice;
-        onView(withId(R.id.total_price_toolbar)).check(matches(withText(getString(mActivityRule, R.string.price, totalCoffeePrice))));
+        changeToCoffeeOrderCountAndVerify(coffeeZoro, ++coffeeCount, totalCoffeePrice, R.id.coffee_increment);
     }
 
     @After
     public void tearDown() {
         unregisterIdlingResources(mServiceIdlingResource);
+    }
+
+    private void changeToCoffeeOrderCountAndVerify(String coffeeName, int coffeeCount, float totalCoffeePrice, int clickedViewId) {
+        clickToViewChildItem(R.id.recyclerView, coffeeName, clickedViewId);
+        checkTextViewCountForCoffee(R.id.recyclerView, R.id.coffee_count, coffeeName, String.valueOf(coffeeCount));
+        onView(withId(R.id.total_price_toolbar)).check(matches(withText(getString(mActivityRule, R.string.price, totalCoffeePrice))));
     }
 }
