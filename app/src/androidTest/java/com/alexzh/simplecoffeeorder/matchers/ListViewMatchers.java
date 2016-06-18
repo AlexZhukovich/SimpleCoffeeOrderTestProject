ee8312da7aeb633bd7883b19a8feb91b3e5d6fe2
Alex <zhukovich.a@gmail.com>
2016-06-18 19:42:54 +0200

    Implemented matcher for coffee verification in list view

---
+++ app/src/androidTest/java/com/alexzh/simplecoffeeorder/matchers/ListViewMatchers.java
@@ -0,0 +1,27 @@
+package com.alexzh.simplecoffeeorder.matchers;
+
+import android.support.test.espresso.matcher.BoundedMatcher;
+
+import com.alexzh.simplecoffeeorder.model.Coffee;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+
+public class ListViewMatchers {
+
+    public static Matcher<Object> withCoffee(final Coffee coffee) {
+        return new BoundedMatcher<Object, Coffee>(Coffee.class) {
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("has value: " + coffee.toString());
+            }
+
+            @Override
+            protected boolean matchesSafely(Coffee item) {
+                return item.equals(coffee);
+            }
+        };
+    }
+
+}
