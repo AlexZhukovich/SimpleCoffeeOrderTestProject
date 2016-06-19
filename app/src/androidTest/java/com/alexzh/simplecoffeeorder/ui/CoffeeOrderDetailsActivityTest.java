dbeef18f2825484d6b3b967033b16a0bbbb19968
Alex <zhukovich.a@gmail.com>
2016-06-19 19:04:07 +0200

    Updated the shouldOpenDetailActivity test

--- app/src/androidTest/java/com/alexzh/simplecoffeeorder/ui/CoffeeOrderDetailsActivityTest.java
+++ app/src/androidTest/java/com/alexzh/simplecoffeeorder/ui/CoffeeOrderDetailsActivityTest.java
@@ -22,13 +22,15 @@ import java.util.List;
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static com.alexzh.simplecoffeeorder.matchers.ListViewMatcher.withCoffee;
 import static com.alexzh.simplecoffeeorder.utils.StringUtils.getString;
-import static org.hamcrest.Matchers.anything;
 
 @RunWith(AndroidJUnit4.class)
 public class CoffeeOrderDetailsActivityTest {
+    private final static int ESPRESSO_INDEX = 0;
 
     private List<Coffee> mCoffeeList;
 
@@ -52,7 +54,11 @@ public class CoffeeOrderDetailsActivityTest {
         Intent intent = CoffeeOrderDetailsActivity.createIntent(targetContext, coffeeOrderMap);
         mActivityRule.launchActivity(intent);
 
-        onData(anything()).atPosition(0).onChildView(withId(R.id.coffee_name)).check(matches(withText("Espresso")));
+        Coffee espresso = CoffeeService.getCoffeeData().get(ESPRESSO_INDEX);
+
+        onData(withCoffee(espresso))
+                .inAdapterView(withId(R.id.detail_list))
+                .check(matches(isDisplayed()));
         onView(withId(R.id.total_price_toolbar)).check(matches(withText(getString(mActivityRule, R.string.price, totalPrice))));
     }
 }
