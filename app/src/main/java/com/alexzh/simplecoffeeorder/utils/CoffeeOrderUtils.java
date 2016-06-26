bbfc783c14fed90bab341b9e40d8cb2fcf89bcf1
Alex <zhukovich.a@gmail.com>
2016-06-26 22:13:41 +0200

    Refactor CoffeeOrderUtils

--- app/src/main/java/com/alexzh/simplecoffeeorder/utils/CoffeeOrderUtils.java
+++ app/src/main/java/com/alexzh/simplecoffeeorder/utils/CoffeeOrderUtils.java
@@ -6,9 +6,9 @@ import java.util.TreeMap;
 
 public class CoffeeOrderUtils {
 
-    public static Coffee getCoffeeByPosition(TreeMap<Coffee, Integer> orderedHashMap, int position) {
+    public static Coffee getCoffeeByPosition(TreeMap<Coffee, Integer> orderedMap, int position) {
         int interactionPosition = 0;
-        for (Coffee key : orderedHashMap.keySet()) {
+        for (Coffee key : orderedMap.keySet()) {
             if (interactionPosition == position) {
                 return  key;
             }
@@ -17,10 +17,10 @@ public class CoffeeOrderUtils {
         return null;
     }
 
-    public static int getCountByCoffee(TreeMap<Coffee, Integer> orderedHashMap, Coffee coffee) {
-        for (Coffee key : orderedHashMap.keySet()) {
+    public static int getCountByCoffee(TreeMap<Coffee, Integer> orderedMap, Coffee coffee) {
+        for (Coffee key : orderedMap.keySet()) {
             if (key.equals(coffee)) {
-                return  orderedHashMap.get(key);
+                return  orderedMap.get(key);
             }
         }
         return 0;
